57b471bd20f7745f6a62e62b7f2d01ff81f1413b
Sumeet Boga <sumeetboga95@gmail.com>
2025-11-17 00:16:23 +0530

    ++

--- functions.php
+++ functions.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 // Format numbers in millions notation (all amounts in USD)
 function formatInr(float|int $num): string
 {
-	$absNum = abs($num);
-	if ($absNum >= 1000000) { // 1 Million
-		return round($num / 1000000, 2) . 'M';
-	} else {
-		return number_format(round($num), 0);
-	}
+	// $absNum = abs($num);
+	// if ($absNum >= 1000000) { // 1 Million
+	// 	return round($num / 1000000, 2) . 'M';
+	// } else {
+	// 	return number_format(round($num), 0);
+	// }
+	
+    return number_format(round($num), 0);
 }
 ?>
