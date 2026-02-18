aedb66b069bd326540f6559398a8d48dbf05e194
Sumeet Boga <sumeetboga95@gmail.com>
2026-02-18 22:21:50 +0530

    ++

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let currentCurrency = 'INR';
 const currencyConfig = {
     'INR': { locale: 'en-IN', symbol: '₹' },
-    'USD': { locale: 'en-US', symbol: '$' },
-    'EUR': { locale: 'en-IE', symbol: '€' }
+    'USD': { locale: 'en-IN', symbol: '$' }, // Force Indian grouping (1,00,000)
+    'EUR': { locale: 'en-IN', symbol: '€' }  // Force Indian grouping
 };
 
 function updateCurrency(newCurrency) {
